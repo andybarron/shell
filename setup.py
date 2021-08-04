d4f9f6fe6c3491319c83c7fbea303e240c6fd35b
Andy Barron <andrewLbarron@gmail.com>
2021-08-03 17:30:34 -0700

    add kubectl plugin

--- setup.py
+++ setup.py
@@ -24,6 +24,7 @@ antigen use oh-my-zsh
 # default oh-my-zsh plugins
 antigen bundle git
 antigen bundle asdf
+antigen bundle kubectl
 
 # third-party oh-my-zsh plugins
 antigen bundle zsh-users/zsh-autosuggestions
